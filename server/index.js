5f5145222b4a5e2e1cd12013ce67186c6e7292a9
rajatm17 <93270086+rajatm17@users.noreply.github.com>
2023-07-19 22:42:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use(cors());
 
