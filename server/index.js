01907057ca55990e3491345ac4cd91570284ca28
rajatm17 <93270086+rajatm17@users.noreply.github.com>
2023-07-19 22:20:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ const { userRouter } = require('./routes/userRoute');
 const { recipeRouter } = require('./routes/recipeRoute');
 const { recipeModel, recipeSchema } = require('./models/recipeModel');
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB');
 
 app.use(cors({origin:'*',exposedHeaders: 'authorization'}));
 
