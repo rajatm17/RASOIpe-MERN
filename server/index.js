a37283a3d386855802e715e2baa9fdf38c877344
rajatm17 <93270086+rajatm17@users.noreply.github.com>
2023-07-19 21:35:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,6 +15,11 @@ mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors({origin:"https://radiant-marigold-4c7868.netlify.app/",exposedHeaders: 'authorization'}));
 
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
